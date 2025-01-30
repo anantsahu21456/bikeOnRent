93452be7e31235ec1841214ebbd5e8d5ff4a4c83
anantsahu21456 <anantsahu21456@gmail.com>
2025-01-30 20:29:38 +0530

    some update

--- rentalbike_backend/controllers/notification.controller.js
+++ rentalbike_backend/controllers/notification.controller.js
@@ -1,86 +1,86 @@
-import asyncHandler from "../utils/asyncHandler.js";
-import { Notification } from "../models/notification.models.js";
+// import asyncHandler from "../utils/asyncHandler.js";
+// import { Notification } from "../models/notification.models.js";
 
-//& Create a new notification
-const createNotification = asyncHandler(async (req, res) => {
-    const { userId, message } = req.body;
+// //& Create a new notification
+// const createNotification = asyncHandler(async (req, res) => {
+//     const { userId, message } = req.body;
 
-    if (!userId || !message) {
-        return res.status(400).json({ message: "User ID and message are required" });
-    }
+//     if (!userId || !message) {
+//         return res.status(400).json({ message: "User ID and message are required" });
+//     }
 
-    const notification = await Notification.create({
-        userId,
-        message,
-    });
+//     const notification = await Notification.create({
+//         userId,
+//         message,
+//     });
 
-    return res.status(201).json({
-        message: "Notification created successfully",
-        notification,
-    });
-});
+//     return res.status(201).json({
+//         message: "Notification created successfully",
+//         notification,
+//     });
+// });
 
-//& Get all notifications for a user
-const getNotifications = asyncHandler(async (req, res) => {
-    const { userId } = req.params;
+// //& Get all notifications for a user
+// const getNotifications = asyncHandler(async (req, res) => {
+//     const { userId } = req.params;
 
-    if (!userId) {
-        return res.status(400).json({ message: "User ID is required" });
-    }
+//     if (!userId) {
+//         return res.status(400).json({ message: "User ID is required" });
+//     }
 
-    const notifications = await Notification.find({ userId }).sort({ createdAt: -1 });
+//     const notifications = await Notification.find({ userId }).sort({ createdAt: -1 });
 
-    if (!notifications.length) {
-        return res.status(404).json({ message: "No notifications found for this user" });
-    }
+//     if (!notifications.length) {
+//         return res.status(404).json({ message: "No notifications found for this user" });
+//     }
 
-    return res.status(200).json({ notifications });
-});
+//     return res.status(200).json({ notifications });
+// });
 
-//& Mark a notification as read
-const markAsRead = asyncHandler(async (req, res) => {
-    const { notificationId } = req.params;
+// //& Mark a notification as read
+// const markAsRead = asyncHandler(async (req, res) => {
+//     const { notificationId } = req.params;
 
-    if (!notificationId) {
-        return res.status(400).json({ message: "Notification ID is required" });
-    }
+//     if (!notificationId) {
+//         return res.status(400).json({ message: "Notification ID is required" });
+//     }
 
-    const notification = await Notification.findByIdAndUpdate(
-        notificationId,
-        { isRead: true },
-        { new: true }
-    );
+//     const notification = await Notification.findByIdAndUpdate(
+//         notificationId,
+//         { isRead: true },
+//         { new: true }
+//     );
 
-    if (!notification) {
-        return res.status(404).json({ message: "Notification not found" });
-    }
+//     if (!notification) {
+//         return res.status(404).json({ message: "Notification not found" });
+//     }
 
-    return res.status(200).json({
-        message: "Notification marked as read",
-        notification,
-    });
-});
+//     return res.status(200).json({
+//         message: "Notification marked as read",
+//         notification,
+//     });
+// });
 
-//& Delete a notification
-const deleteNotification = asyncHandler(async (req, res) => {
-    const { notificationId } = req.params;
+// //& Delete a notification
+// const deleteNotification = asyncHandler(async (req, res) => {
+//     const { notificationId } = req.params;
 
-    if (!notificationId) {
-        return res.status(400).json({ message: "Notification ID is required" });
-    }
+//     if (!notificationId) {
+//         return res.status(400).json({ message: "Notification ID is required" });
+//     }
 
-    const notification = await Notification.findByIdAndDelete(notificationId);
+//     const notification = await Notification.findByIdAndDelete(notificationId);
 
-    if (!notification) {
-        return res.status(404).json({ message: "Notification not found" });
-    }
+//     if (!notification) {
+//         return res.status(404).json({ message: "Notification not found" });
+//     }
 
-    return res.status(200).json({ message: "Notification deleted successfully" });
-});
+//     return res.status(200).json({ message: "Notification deleted successfully" });
+// });
 
-export {
-    createNotification,
-    getNotifications,
-    markAsRead,
-    deleteNotification,
-};
+// export {
+//     createNotification,
+//     getNotifications,
+//     markAsRead,
+//     deleteNotification,
+// };
--- rentalbike_backend/routes/booking.routes.js
+++ rentalbike_backend/routes/booking.routes.js
@@ -1,18 +1,18 @@
-import { Router } from "express";
-const router  = Router()
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+// import { Router } from "express";
+// const router  = Router()
+// import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-import {createBooking, getAllBookings, getBookingById,updateBookingStatus, deleteBooking, getBookingsByUser
-} from "../controllers/booking.controller.js";
-//& Booking Routes
-router.route('/createBooking').post(verifyJWT, createBooking)
-router.route('/allBooking').get(verifyJWT, getAllBookings)
-router.route('/booking/:id').get(verifyJWT, getBookingById)
-router.route('/booking-status/:id/status').patch(verifyJWT, updateBookingStatus)
-router.route('/delete-booking/:id').delete(verifyJWT, deleteBooking)
-router.route('/book-by-user/:userId').get(verifyJWT, getBookingsByUser)
-
-
+// import {createBooking, getAllBookings, getBookingById,updateBookingStatus, deleteBooking, getBookingsByUser
+// } from "../controllers/booking.controller.js";
+// //& Booking Routes
+// router.route('/createBooking').post(verifyJWT, createBooking)
+// router.route('/allBooking').get(verifyJWT, getAllBookings)
+// router.route('/booking/:id').get(verifyJWT, getBookingById)
+// router.route('/booking-status/:id/status').patch(verifyJWT, updateBookingStatus)
+// router.route('/delete-booking/:id').delete(verifyJWT, deleteBooking)
+// router.route('/book-by-user/:userId').get(verifyJWT, getBookingsByUser)
 
 
-export default router
+
+
+// export default router
